ea515222da314f1576dac995ff0c0766321dacf5
giangdhptit <gianglibra1710@gmail.com>
2024-06-05 23:25:41 +0700

    users, setup meeting, email

--- src/main/java/com/example/demo/service/impl/ClassroomServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ClassroomServiceImpl.java
@@ -3,13 +3,17 @@ package com.example.demo.service.impl;
 import com.example.demo.entity.Classroom;
 import com.example.demo.repository.ClassroomRepository;
 import com.example.demo.service.ClassroomService;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Properties;
 
 @Service
 @RequiredArgsConstructor
@@ -35,7 +39,36 @@ public class ClassroomServiceImpl implements ClassroomService {
 
     @Override
     public void sendSimpleEmail(String to, String subject, String text) {
+//        Properties prop = new Properties();
+//        prop.put("mail.smtp.host", "smtp.gmail.com");
+//        prop.put("mail.smtp.port", "587");
+//        prop.put("mail.smtp.auth", "true");
+//        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+//
+//        Session session = Session.getInstance(prop,
+//                new ja.mail.Authenticator() {
+//                    protected PasswordAuthentication getPasswordAuthentication() {
+//                        return new PasswordAuthentication(username, password);
+//                    }
+//                });
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.zoho.com");
+        mailSender.setPort(465);  // or 587 for TLS
+        mailSender.setUsername("giang.dinh@lifesup.com.vn");
+        mailSender.setPassword("ESjYL2shH66S");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.ssl.enable", "true");  // Use this for SSL
+        // props.put("mail.smtp.starttls.enable", "true");  // Uncomment for TLS
+        props.put("mail.smtp.connectiontimeout", "5000");
+        props.put("mail.smtp.timeout", "5000");
+        props.put("mail.smtp.writetimeout", "5000");
+//        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        props.put("mail.smtp.ssl.trust", "smtp.zoho.com");
+        props.put("mail.smtp.starttls.enable", "true");
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("giang.dinh@lifesup.com.vn");
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
